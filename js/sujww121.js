a747653bbbee8ee6ec591c8d8f043faccfa78fab
unknown <chlalstn9@gmail.com>
2024-11-25 23:07:31 +0900

    update

--- js/sujww121.js
+++ js/sujww121.js
@@ -1,8 +1,8 @@
 var jsonldData = {
   "@context": "https://schema.org",
   "@type": "Organization",
-  url: "https://geumcheon.jianhomecare.com",
-  logo: "https://geumcheon.jianhomecare.com/img/변기막힘하수구막힘.png",
+  url: "https://gangdong.jianhomecare.com",
+  logo: "https://gangdong.jianhomecare.com/img/변기막힘하수구막힘.png",
 };
 
 var script = document.createElement("script");
